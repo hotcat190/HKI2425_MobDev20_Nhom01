bbb27f6ba66acd3886f247d6866bad8f56f26e75
Le Vu Viet Anh <vietanhlevu2004@gmail.com>
2024-11-12 16:56:07 +0700

    Category Screen update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ android {
 dependencies {
 
 
+
+
     implementation("io.coil-kt:coil-compose:2.0.0")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")

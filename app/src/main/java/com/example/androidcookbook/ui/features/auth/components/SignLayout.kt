28f7acc7c9f0f34667dfcf0b09efd4c179de33a4
DuongMinhHoang <22021218@vnu.edu.vn>
2024-11-24 18:14:58 +0700

    Fix SignLayout

--- app/src/main/java/com/example/androidcookbook/ui/features/auth/components/SignLayout.kt
+++ app/src/main/java/com/example/androidcookbook/ui/features/auth/components/SignLayout.kt
@@ -35,8 +35,8 @@ fun SignLayout(
         Canvas(modifier = Modifier.fillMaxSize()) {
             drawOval(
                 color = Color(0xFF4F3423),
-                topLeft = Offset(size.width * (-0.1f), size.height / (-14)),
-                size = Size(size.width * 1.2f, size.height / 4)
+                topLeft = Offset(-215f, -290f),
+                size = Size(1500f, 750f)
             )
         }
 

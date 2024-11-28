bffa824b8d7675c7f032dee35e727faf090ac388
DuongMinhHoang <22021218@vnu.edu.vn>
2024-11-28 17:21:11 +0700

    Search Screen
    Khi Search thì sẽ hiện lên những thẻ món ăn có chứa tên, thể loại, quốc gia

--- app/src/main/java/com/example/androidcookbook/ui/features/search/SearchScreen.kt
+++ app/src/main/java/com/example/androidcookbook/ui/features/search/SearchScreen.kt
@@ -1,9 +1,43 @@
 package com.example.androidcookbook.ui.features.search
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.androidcookbook.R
+import com.example.androidcookbook.domain.model.recipe.Recipe
 
 @Composable
 fun SearchScreen(
@@ -12,6 +46,105 @@ fun SearchScreen(
     modifier: Modifier = Modifier,
 ) {
     BackHandler { onBackButtonClick() }
-    Text(text = searchUiState.result)
+    if (searchUiState.fail) {
+        Text(
+            text = searchUiState.result,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(10.dp),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center
+        )
+    } else {
+        LazyColumn(
+            modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            items(searchUiState.resultList) {
+                    item -> ResultCard(item)
+            }
+        }
+    }
 }
 
+@Composable
+fun ResultCard(
+    recipe: Recipe
+) {
+    Card(
+        onClick = {},
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(2.dp, Color(0xFFFFD09B)),
+        colors = CardDefaults.cardColors(Color(0xFFFFF7D1))
+    ) {
+        Row(
+            modifier = Modifier.height(120.dp)
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f),
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data(recipe.strMealThumb)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                error = painterResource(id = R.drawable.ic_broken_image),
+                placeholder = painterResource(id = R.drawable.loading_img)
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = recipe.strMeal,
+                    modifier = Modifier
+                        .weight(2f)
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Start
+                )
+                Text(
+                    text = recipe.strCategory,
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth(),
+                    fontSize = 18.sp,
+                    fontStyle = FontStyle.Italic
+                )
+                Text(
+                    text = recipe.strArea,
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CardPreview() {
+    ResultCard(
+        Recipe(
+            0,
+            "Summer Dish",
+            "Side",
+            "Japanese",
+            "",
+            "")
+    )
+}
--- app/src/main/java/com/example/androidcookbook/ui/features/search/SearchUiState.kt
+++ app/src/main/java/com/example/androidcookbook/ui/features/search/SearchUiState.kt
@@ -1,6 +1,11 @@
 package com.example.androidcookbook.ui.features.search
 
+import com.example.androidcookbook.domain.model.recipe.Recipe
+import com.example.androidcookbook.domain.model.recipe.RecipeList
+
 data class SearchUiState(
     val searchQuery: String = "",
     val result: String = "",
+    val resultList: List<Recipe> = listOf(),
+    val fail: Boolean = false
 )

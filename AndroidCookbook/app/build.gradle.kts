846bfd3cab4a83eaaffa3f4bd0f22ad6aa3899f0
hotcat190 <nguyendinhnguyen151204@gmail.com>
2024-11-30 20:56:33 +0700

    Update min sdk

--- AndroidCookbook/app/build.gradle.kts
+++ AndroidCookbook/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.androidcookbook"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"

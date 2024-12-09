139d222ff1c3c66721ca55fef615611835376471
Le Vu Viet Anh <vietanhlevu2004@gmail.com>
2024-12-09 21:37:28 +0700

    update notification

--- AndroidCookbook/app/src/main/java/com/example/androidcookbook/MainActivity.kt
+++ AndroidCookbook/app/src/main/java/com/example/androidcookbook/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.androidcookbook
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -13,13 +17,29 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import com.example.androidcookbook.ui.CookbookApp
+import com.example.androidcookbook.ui.components.FirebaseMessagingScreen
 import com.example.androidcookbook.ui.theme.AndroidCookbookTheme
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FCM", "Fetching FCm registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+            val token = task.result
+
+            Log.d("FCM", token.toString())
+        })
+
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge(
             navigationBarStyle = SystemBarStyle.auto(
--- AndroidCookbook/app/src/main/java/com/example/androidcookbook/ui/features/auth/AuthViewModel.kt
+++ AndroidCookbook/app/src/main/java/com/example/androidcookbook/ui/features/auth/AuthViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.androidcookbook.ui.features.auth
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.androidcookbook.data.repositories.AuthRepository
 import com.example.androidcookbook.domain.model.auth.RegisterRequest
 import com.example.androidcookbook.domain.model.auth.RegisterResponse
+import com.example.androidcookbook.domain.model.auth.SetTokenRequest
 import com.example.androidcookbook.domain.model.auth.SignInRequest
 import com.example.androidcookbook.domain.model.auth.SignInResponse
 import com.example.androidcookbook.domain.network.ErrorBody
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.skydoves.sandwich.onException
 import com.skydoves.sandwich.onSuccess
 import com.skydoves.sandwich.retrofit.serialization.onErrorDeserialize
@@ -80,15 +84,48 @@ class AuthViewModel @Inject constructor(
             // Send request and receive the response
             val response = authRepository.login(SignInRequest(username, password))
             response.onSuccess {
-                _uiState.update { it.copy(openDialog = true, dialogMessage = data.message, signInSuccess = true) }
+                _uiState.update {
+                    it.copy(
+                        openDialog = true,
+                        dialogMessage = data.message,
+                        signInSuccess = true
+                    )
+                }
+                sendSetToken()
                 onSuccess(data)
             }.onErrorDeserialize<SignInResponse, ErrorBody> { errorBody ->
-                _uiState.update { it.copy(openDialog = true, dialogMessage = errorBody.message.joinToString(".\n\n")) }
+                _uiState.update {
+                    it.copy(
+                        openDialog = true,
+                        dialogMessage = errorBody.message.joinToString(".\n\n")
+                    )
+                }
             }.onException {
                 when (throwable) {
-                    is SocketTimeoutException -> _uiState.update { it.copy(openDialog = true, dialogMessage = "Request timed out.\n Please try again.") }
+                    is SocketTimeoutException -> _uiState.update {
+                        it.copy(
+                            openDialog = true,
+                            dialogMessage = "Request timed out.\n Please try again."
+                        )
+                    }
                 }
             }
         }
+    }
+
+    fun sendSetToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FCM", "Fetching FCm registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+            val token = task.result
+            viewModelScope.launch {
+                authRepository.sendSetTokenRequest(setTokenRequest = SetTokenRequest(token = token))
+            }
+            Log.d("FCM", token.toString())
+        })
+
+
     }
 }

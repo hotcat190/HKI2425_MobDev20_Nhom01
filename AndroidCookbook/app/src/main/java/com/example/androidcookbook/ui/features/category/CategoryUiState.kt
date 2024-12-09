baaa8df6f26881c20cac822cae43b48551122403
Le Vu Viet Anh <vietanhlevu2004@gmail.com>
2024-12-10 00:42:18 +0700

    hot fix saving login session

--- AndroidCookbook/app/src/main/java/com/example/androidcookbook/ui/features/category/CategoryUiState.kt
+++ AndroidCookbook/app/src/main/java/com/example/androidcookbook/ui/features/category/CategoryUiState.kt
@@ -1,9 +1,10 @@
 package com.example.androidcookbook.ui.features.category
 
 import com.example.androidcookbook.domain.model.category.Category
+import com.example.androidcookbook.domain.model.recipe.Recipe
 
 sealed interface CategoryUiState{
-    data class Success(val categories: List<Category>) : CategoryUiState
+    data class Success(val categories: List<Category>, val randomMeals: List<Recipe>) : CategoryUiState
     data object Error : CategoryUiState
     data object Loading : CategoryUiState
 }
